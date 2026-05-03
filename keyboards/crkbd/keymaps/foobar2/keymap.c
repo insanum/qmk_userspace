b517e68358704065f5d7c40b9e9612b9fd8eda39
Eric Davis <edavis@insanum.com>
2026-05-03 12:28:04 -0700

    added left alt for gaming layer

--- keyboards/crkbd/keymaps/foobar2/keymap.c
+++ keyboards/crkbd/keymaps/foobar2/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G, /****/ KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
         KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B, /****/ KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_BSLS,
 
-            LGUI_T(KC_SPC), KC_SPC, LSFT_T(KC_ESC), /****/ RSFT_T(KC_ENT), LT(_FUNCS,KC_SPC), MO(_SYMBOLS)
+            LGUI_T(KC_SPC), LALT_T(KC_SPC), LSFT_T(KC_ESC), /****/ RSFT_T(KC_ENT), LT(_FUNCS,KC_SPC), MO(_SYMBOLS)
     ),
 
     [2] /* _SYMBOLS */ = LAYOUT_split_3x6_3(
